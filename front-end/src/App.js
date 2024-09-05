b126caced43db2b2ed87bb86fcec94a9b16690ef
Mohamed Ibrahim <sallamm733@gmail.com>
2024-09-05 13:32:42 +0300

    login and register front done

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,16 +1,17 @@
 import "./App.css";
-import Post from "./components/post";
-import Header from "./components/header";
 import { Route, Routes } from "react-router-dom";
 import Layout from "./layout/layout";
 import MainPage from "./pages/main";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<MainPage />} />
-        <Route path="login" element={<div>Login</div>} />
+        <Route path="login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Route>
     </Routes>
   );
