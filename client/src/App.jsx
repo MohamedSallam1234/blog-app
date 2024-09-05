1cceaa4c3444b5444b812a8fdd4c9c42d5392694
momen yasser <momenyasser@itworx.com>
2024-09-05 10:53:30 +0300

    chore: Update routing in App component and create LoginPage and RegisterPage components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,16 @@
 import Homepage from './components/Homepage'
 import { Route, Routes } from 'react-router-dom'
 import Layout from './components/Layout'
+import LoginPage from './components/LoginPage'
+import RegisterPage from './components/RegisterPage'
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Homepage />} />
-        <Route path="/login" element={<h1>Login</h1>} />
-        <Route path="/register" element={<h1>Register </h1>} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Route>
     </Routes>
   )
--- client/src/components/RegisterPage.jsx
+++ client/src/components/RegisterPage.jsx
@@ -1,5 +1,25 @@
 const RegisterPage = () => {
-  return <h1>Register</h1>
+  return (
+    <div className="flex flex-col justify-center items-center pb-[12rem] w-screen h-screen gap-[2rem]">
+      <p className="text-3xl font-bold text-center">Register</p>
+      <input
+        className="w-[31rem] px-4 py-2 border border-gray-300 rounded-lg focus:outline-gray-500"
+        type="text"
+        placeholder="Username"
+      />
+      <input
+        className="w-[31rem] px-4 py-2 border border-gray-300 rounded-lg focus:outline-gray-500"
+        type="password"
+        placeholder="Password"
+      />
+      <button
+        className="w-[31rem] bg-gray-600 text-white py-2 rounded-lg hover:bg-gray-500 transition duration-300"
+        type="submit"
+      >
+        Register
+      </button>
+    </div>
+  )
 }
 
 export default RegisterPage
